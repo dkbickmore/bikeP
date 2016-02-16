1e5917942a88ffe654000d5125c433af23e2f2c5
dkbickmore <dkbickmore@gmail.com>
2016-02-15 20:47:58 -0700

    Bike game menu Practice

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("new scene load " + name);
+		Application.LoadLevel (name);
+	}
+}
